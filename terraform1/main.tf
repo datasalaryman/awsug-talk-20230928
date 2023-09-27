930a0d1f11147ddbe26c3cc19824fc026859ed8e
Data Salaryman <datasalaryman@protonmail.com>
2023-09-28 01:14:54 +0800

    feat(terraform1): add first terraform example

---
+++ terraform1/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "ap-southeast-1"
+  # profile = "personal" -optional if you have multiple AWS profiles
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  # bucket = "awsug_sample_bucket" -- will just tell terraform to pick a random name
+}
